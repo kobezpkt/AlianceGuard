db4888df9b30dfd473bea8dc6d73d7a039e8bf22
kobezpkt <StarPlatinun@proton.me>
2026-01-06 15:03:23 -0300

    Add files via upload

---
+++ AlianceGuard/Config.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace AlianceGuard
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public bool Debug { get; set; } = false;
+
+
+    }
+}
