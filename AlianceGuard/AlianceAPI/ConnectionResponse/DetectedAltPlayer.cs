334d55acb65893f606fdda6953e35935dc536fe2
kobezpkt <StarPlatinun@proton.me>
2026-02-20 16:43:46 -0300

    Add files via upload

---
+++ AlianceGuard/AlianceAPI/ConnectionResponse/DetectedAltPlayer.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace AlianceGuard.AlianceAPI.ConnectionResponse;
+
+public class DetectedAltPlayer
+{
+    [JsonProperty("steam_id64")]
+    public string SteamId64 { get; set; }
+
+    [JsonProperty("player_name")]
+    public string PlayerName { get; set; }
+}
